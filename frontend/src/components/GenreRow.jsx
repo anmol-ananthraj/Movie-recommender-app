0c241f836988b1bf012846fef01c3e10f837dbd5
Anmol Ananthraj <anmolananthraj20@gmail.com>
2026-04-14 19:33:06 +0530

    Add scroll buttons for movie genre navigation in GenreRow component

--- frontend/src/components/GenreRow.jsx
+++ frontend/src/components/GenreRow.jsx
@@ -1,9 +1,23 @@
 import MovieCard from "./MovieCard"
 import { useRef } from "react"
+import leftIcon from "../assets/scrollLeft.png";
+import rightIcon from "../assets/scrollRight.png";
+
 
 function GenreRow({ title, movies }) {
   const scrollRef = useRef(null)
-
+  const scrollLeft = () => {
+  scrollRef.current.scrollBy({
+    left: -500,
+    behavior: "smooth"
+  })
+  }
+  const scrollRight = () => {
+  scrollRef.current.scrollBy({
+    left: 500,
+    behavior: "smooth"
+  })
+  }
   return (
     <section>
       <div className="flex justify-between items-end mb-8">
@@ -11,15 +25,34 @@ function GenreRow({ title, movies }) {
           <h2 className="text-2xl font-black font-headline tracking-tight text-on-surface">{title}</h2>
           <div className="h-1 w-12 bg-primary mt-2"></div>
         </div>
-        <a className="text-xs font-bold font-headline uppercase tracking-widest text-on-surface/40 hover:text-primary transition-colors cursor-pointer">
-          See all matches
-        </a>
       </div>
-      
-      <div className="flex gap-6 overflow-x-auto no-scrollbar pb-8 -mx-4 px-4 scroll-smooth" ref={scrollRef}>
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      <div className="relative">
+        {/* LEFT BUTTON */}
+        <button
+          onClick={scrollLeft}
+          className="absolute left-[-55px] top-[42%] -translate-y-1/2 z-10
+                 w-12 h-12 rounded-[50%] 
+                 flex items-center justify-center 
+                 shadow-2xl"
+        >
+          <img src={leftIcon} alt="left" className="w-6 h-8 ml-[5px] invert brightness-200" />
+        </button>
+
+        {/* RIGHT BUTTON */}
+        <button
+          onClick={scrollRight}
+          className="absolute right-[-65px] top-[42%] -translate-y-1/2 z-10
+                 w-12 h-12 rounded-[50%] 
+                 flex items-center justify-center 
+                 shadow-2xl"
+        >
+          <img src={rightIcon} alt="right" className="w-6 h-8 mr-[4px] invert brightness-200" />
+        </button>
+        <div className="flex gap-6 overflow-x-auto no-scrollbar pb-8 -mx-4 px-4 scroll-smooth" ref={scrollRef}>
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
       </div>
     </section>
   )
